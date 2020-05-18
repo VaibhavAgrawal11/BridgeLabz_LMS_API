855c072274fad24a62d2955b7752beff709f8c6b
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-05-18 07:56:43 +0530

    ADDED: Login response pattern DTO.

---
+++ src/main/java/com/bridgelaz/bridgelabzlms/dto/LoginDTO.java
@@ -0,0 +1,27 @@
+package com.bridgelaz.bridgelabzlms.dto;
+
+public class LoginDTO {int status;
+    String message;
+
+    public LoginDTO(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
