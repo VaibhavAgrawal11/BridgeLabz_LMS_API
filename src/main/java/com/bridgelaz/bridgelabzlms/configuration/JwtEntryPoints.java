fb41a8fd5ed1d67a55aaeb2120cd9fa9999a13df
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-05-18 07:47:02 +0530

    ADDED: Configured JWT Entry points.

--- src/main/java/com/bridgelaz/bridgelabzlms/configuration/JwtEntryPoints.java
+++ src/main/java/com/bridgelaz/bridgelabzlms/configuration/JwtEntryPoints.java
@@ -1,4 +1,22 @@
 package com.bridgelaz.bridgelabzlms.configuration;
 
-public class JwtEntryPoints {
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+@Component
+public class JwtEntryPoints implements AuthenticationEntryPoint, Serializable {
+    private static final long serialVersionUID = -7858869558953243875L;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
 }
