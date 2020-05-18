aba18f741614f49673f4fbf916ef60a8a4a449bf
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-05-18 07:48:34 +0530

    ADDED: Http controller to register.

--- src/main/java/com/bridgelaz/bridgelabzlms/contoller/BridgeLabz_LMS_Controller.java
+++ src/main/java/com/bridgelaz/bridgelabzlms/contoller/BridgeLabz_LMS_Controller.java
@@ -1,13 +1,23 @@
 package com.bridgelaz.bridgelabzlms.contoller;
 
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bridgelaz.bridgelabzlms.dto.UserDTO;
+import com.bridgelaz.bridgelabzlms.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BridgeLabz_LMS_Controller {
+    @Autowired
+    private UserServiceImpl userService;
+
     @RequestMapping({"/hello", "", "/"})
     public String displayHomePage() {
         return "Welcome to Bridgelabz LMS API development project.";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
+        return ResponseEntity.ok(userService.save(user));
     }
 }
