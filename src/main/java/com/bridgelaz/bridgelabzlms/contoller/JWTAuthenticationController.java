b2eae28fe1c397094ab9220268e06d24826f1bcb
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-05-18 07:51:47 +0530

    ADDED: Jwt controller to generate jwt while login.

--- src/main/java/com/bridgelaz/bridgelabzlms/contoller/JWTAuthenticationController.java
+++ src/main/java/com/bridgelaz/bridgelabzlms/contoller/JWTAuthenticationController.java
@@ -1,4 +1,50 @@
 package com.bridgelaz.bridgelabzlms.contoller;
 
+import com.bridgelaz.bridgelabzlms.dto.RequestJwt;
+import com.bridgelaz.bridgelabzlms.dto.ResponseJwt;
+import com.bridgelaz.bridgelabzlms.service.JwtUserService;
+import com.bridgelaz.bridgelabzlms.util.Token;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
 public class JWTAuthenticationController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private Token jwtTokenUtil;
+
+    @Autowired
+    private JwtUserService userDetailsService;
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody RequestJwt authenticationRequest) throws Exception {
+
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new ResponseJwt(token));
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
 }
