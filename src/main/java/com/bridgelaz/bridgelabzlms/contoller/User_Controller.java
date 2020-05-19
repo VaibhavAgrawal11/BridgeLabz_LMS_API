f69755df48eeacab2937f080860b591af809e66a
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-05-19 10:40:13 +0530

    ADDED: forgotPassword and resetPassword endpoints.

--- src/main/java/com/bridgelaz/bridgelabzlms/contoller/User_Controller.java
+++ src/main/java/com/bridgelaz/bridgelabzlms/contoller/User_Controller.java
@@ -1,8 +1,8 @@
 package com.bridgelaz.bridgelabzlms.contoller;
 
-import com.bridgelaz.bridgelabzlms.dto.LoginRequest;
-import com.bridgelaz.bridgelabzlms.dto.LoginResponse;
-import com.bridgelaz.bridgelabzlms.dto.UserDTO;
+import com.bridgelaz.bridgelabzlms.dto.*;
+import com.bridgelaz.bridgelabzlms.models.User;
+import com.bridgelaz.bridgelabzlms.repository.UserRepository;
 import com.bridgelaz.bridgelabzlms.service.UserServiceImpl;
 import com.bridgelaz.bridgelabzlms.util.Token;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +14,19 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.validation.constraints.Email;
+
 @RestController
 public class User_Controller {
     @Autowired
     private UserServiceImpl userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -29,12 +38,12 @@ public class User_Controller {
         return "Welcome to Bridgelabz LMS API development project.";
     }
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
+    @RequestMapping(value = "/user/register", method = RequestMethod.POST)
+    public ResponseEntity<?> saveUser(@Valid @RequestBody UserDTO user) throws Exception {
         return ResponseEntity.ok(userService.save(user));
     }
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @RequestMapping(value = "/user/login", method = RequestMethod.POST)
     public ResponseEntity<?> createAuthenticationToken(@RequestBody LoginRequest authenticationRequest) throws Exception {
         try {
             authenticationManager.authenticate
@@ -52,5 +61,21 @@ public class User_Controller {
         final String token = jwtTokenUtil.generateToken(userDetails);
 
         return ResponseEntity.ok(new LoginResponse(token));
+    }
+
+    @GetMapping("/forgetPassword")
+    public UserResponse requestResetPassword(@Valid @RequestParam @Email String emailAddress) throws AddressException, MessagingException {
+        User user = userRepository.findByEmail(emailAddress);
+        final String token = jwtTokenUtil.generatePasswordResetToken(String.valueOf(user.getId()));
+        userService.sentEmail(user, token);
+        return new UserResponse(200, token);
+    }
+
+    @PutMapping("/user/resetPassword")
+    public UserResponse resetPassword(@Valid @RequestBody ResetPassword resetPassword) {
+        boolean result = userService.resetPassword(resetPassword.getPassword(), resetPassword.getToken());
+        if (result)
+            return new UserResponse(200, "Successfully updated");
+        return new UserResponse(500, "UnSuccessFull");
     }
 }
