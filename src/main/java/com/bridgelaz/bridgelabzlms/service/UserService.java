0577df4e44ffd94173750e3ccd70ec6f8e35b201
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-05-19 10:42:16 +0530

    ADDED: Method to sent mail to user and upadate password.

--- src/main/java/com/bridgelaz/bridgelabzlms/service/UserService.java
+++ src/main/java/com/bridgelaz/bridgelabzlms/service/UserService.java
@@ -1,9 +1,16 @@
 package com.bridgelaz.bridgelabzlms.service;
 
-import com.bridgelaz.bridgelabzlms.dto.LoginDTO;
+import com.bridgelaz.bridgelabzlms.dto.UserResponse;
 import com.bridgelaz.bridgelabzlms.dto.UserDTO;
+import com.bridgelaz.bridgelabzlms.models.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import javax.mail.MessagingException;
+
 public interface UserService extends UserDetailsService {
-    public LoginDTO save(UserDTO userDto);
+    public UserResponse save(UserDTO userDto);
+
+    public void sentEmail(User user, String token) throws MessagingException;
+
+    boolean resetPassword(String password, String token);
 }
--- src/main/java/com/bridgelaz/bridgelabzlms/service/UserServiceImpl.java
+++ src/main/java/com/bridgelaz/bridgelabzlms/service/UserServiceImpl.java
@@ -1,16 +1,22 @@
 package com.bridgelaz.bridgelabzlms.service;
 
-import com.bridgelaz.bridgelabzlms.dto.LoginDTO;
+import com.bridgelaz.bridgelabzlms.dto.UserResponse;
 import com.bridgelaz.bridgelabzlms.dto.UserDTO;
 import com.bridgelaz.bridgelabzlms.models.User;
 import com.bridgelaz.bridgelabzlms.repository.UserRepository;
+import com.bridgelaz.bridgelabzlms.util.Token;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
@@ -25,15 +31,25 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private JavaMailSender sender;
+
+    @Autowired
+    private Token jwtToken;
+
+    @Autowired
+    EntityManager entityManager;
+
+
     @Override
-    public LoginDTO save(UserDTO user) {
+    public UserResponse save(UserDTO user) {
         user.setCreator_stamp(LocalDateTime.now());
         user.setCreator_user(user.getFirst_name());
         user.setVerified("yes");
         user.setPassword(bcryptEncoder.encode(user.getPassword()));
         User newUser = modelMapper.map(user, User.class);
         userRepository.save(newUser);
-        return new LoginDTO(200, "successfully Registered");
+        return new UserResponse(200, "successfully Registered");
     }
 
     @Override
@@ -45,4 +61,31 @@ public class UserServiceImpl implements UserService {
         return new org.springframework.security.core.userdetails.User(user.getFirst_name(), user.getPassword(),
                 new ArrayList<>());
     }
+
+    @Override
+    public void sentEmail(User user, String token) throws MessagingException {
+        String recipientAddress = user.getEmail();
+        MimeMessage message = sender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        helper.setTo(recipientAddress);
+        helper.setText("Hii " + user.getFirst_name() + "\n" + " You requested to reset password, if YES then click on link put your new password and NO then ignore");
+        helper.setSubject("Password-Reset-Request");
+        sender.send(message);
+    }
+
+    @Override
+    public boolean resetPassword(String password, String token) {
+        String encodedPassword = bcryptEncoder.encode(password);
+        if (jwtToken.isTokenExpired(token)) {
+            return false;
+        }
+        String username = jwtToken.getSubjectFromToken(token);
+        User user = userRepository.findByFirstName(username);
+        user.setPassword(encodedPassword);
+        User updatedUser = userRepository.save(user);
+        if (updatedUser.getPassword().equalsIgnoreCase(encodedPassword))
+            return true;
+        return false;
+    }
+
 }
