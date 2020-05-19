dfe5e106ca01e747bec712440cea45378851dbeb
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-05-19 10:44:14 +0530

    ADDED:DTO for Request for user to reset password.

---
+++ src/main/java/com/bridgelaz/bridgelabzlms/dto/ResetPassword.java
@@ -0,0 +1,25 @@
+package com.bridgelaz.bridgelabzlms.dto;
+
+import javax.validation.constraints.Pattern;
+
+public class ResetPassword {
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]*.{8,}$")
+    private String password;
+    private String token;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
